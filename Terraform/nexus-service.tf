c7ed6e5d8cd2c18e26c7c5091fdcae0e31b5946d
moh-amer <muhammed.alaa.amer@gmail.com>
2023-07-04 00:17:02 +0300

    final modifications

--- Terraform/nexus-service.tf
+++ Terraform/nexus-service.tf
@@ -27,3 +27,28 @@ resource "kubernetes_service" "nexus_svc" {
   }
 }
 
+
+
+resource "kubernetes_service" "nexus_svc_80" {
+  depends_on = [kubernetes_deployment.nexus]
+  metadata {
+    name      = "nexus-service"
+    namespace = kubernetes_namespace.tools.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "nexus"
+    }
+
+    port {
+      name        = "http"
+      protocol    = "TCP"
+      port        = 80
+      target_port = 5000
+    }
+
+    type = "ClusterIP"
+  }
+}
+
